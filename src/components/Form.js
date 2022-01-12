c1bb5f71adca490440095dd2cc8a3cc6057e7e99
ibrahimerkenov <44646383+ibrahimerkenov@users.noreply.github.com>
2022-01-12 22:54:12 +0300

    Added autofocus on input

--- src/components/Form.js
+++ src/components/Form.js
@@ -15,7 +15,7 @@ const Form = ({ inputText, setInputText, todos, setTodos, setStatus }) => {
       ]);
       setInputText("");
     } else {
-      
+
     }
 
   }
@@ -26,7 +26,7 @@ const Form = ({ inputText, setInputText, todos, setTodos, setStatus }) => {
 
   return (
     <form>
-      <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+      <input autoFocus={true} value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
       <button onClick={submitTodoHandler} className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
